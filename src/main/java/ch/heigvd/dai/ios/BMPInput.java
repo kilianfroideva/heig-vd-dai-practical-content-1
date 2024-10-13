7141e7029b2a4130e07acd5c15085ce4dbbb76fe
nicolasbvd <nicolas.bovard@heig-vd.ch>
2024-10-13 18:26:56 +0200

    Prototype of taking info of a BMP of 24bits

--- src/main/java/ch/heigvd/dai/ios/BMPInput.java
+++ src/main/java/ch/heigvd/dai/ios/BMPInput.java
@@ -1,14 +1,49 @@
 package ch.heigvd.dai.ios;
-import java.io.BufferedInputStream;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 
-/*
-public abstract class BMPInput {
-    public BMP_file getInput(String filename){
-        InputStream file = new FileInputStream(filename);
-        BufferedInputStream bFile = new BufferedInputStream(file);
+import ch.heigvd.dai.bitmap.BMP_file;
+
+// Classe interne pour stocker les informations du BMP
+class BMPInfo {
+    int width;
+    int height;
+    int bitsPerPixel;
+
+    BMPInfo(int width, int height, int bitsPerPixel) {
+        this.width = width;
+        this.height = height;
+        this.bitsPerPixel = bitsPerPixel;
     }
 }
-*/
+
+//Pour bitmap de 24 bits
+public abstract class BMPInput {
+    public BMPInfo getInput(String filename){
+        int width = -1;
+        int height = -1;
+        int bitsPerPixel = -1;
+        try {
+            InputStream file = new FileInputStream(filename);
+            BufferedInputStream bFile = new BufferedInputStream(file);
+
+            byte[] header = new byte[54];  // En-tête BMP de 54 octets
+            bFile.read(header, 0, 54);
+
+            // Largeur (octets 18-21)
+            width = ((header[21] & 0xFF) << 24) | ((header[20] & 0xFF) << 16) |
+                    ((header[19] & 0xFF) << 8) | (header[18] & 0xFF);
+
+            // Hauteur (octets 22-25)
+            height = ((header[25] & 0xFF) << 24) | ((header[24] & 0xFF) << 16) |
+                    ((header[23] & 0xFF) << 8) | (header[22] & 0xFF);
+
+            // Bits par pixel (octets 28-29)
+            bitsPerPixel = ((header[29] & 0xFF) << 8) | (header[28] & 0xFF);
+
+
+        }catch(IOException e){
+        }
+        return new BMPInfo(width, height, bitsPerPixel);
+
+    }
+}
